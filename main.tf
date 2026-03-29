69a66018cae088cee5236a891dfa67a56b7f0bcf
Chris <chodson1@hotmail.co.uk>
2026-03-29 10:41:12 +0100

    Clean initial commit (no terraform cache)

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+	required_providers {
+		azurerm = {
+			source = "hashicorp/azurerm"
+			version = "~> 4.0"
+		}
+	}
+}
+
+provider "azurerm" {
+		features{}
+		subscription_id						= "1416f3d7-88c5-4438-8133-8dd3d4acad4c"
+		resource_provider_registrations		= "none"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ghpro100-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "ghpro100web" # change if needed
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account_static_website" "website" {
+  storage_account_id = azurerm_storage_account.storage.id
+
+  index_document     = "index.html"
+  error_404_document = "404.html"
+}
+
+resource "azurerm_cdn_frontdoor_profile" "afd" {
+  name                = "ghpro100-afd"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Standard_AzureFrontDoor"
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "endpoint" {
+  name                     = "ghpro100-endpoint"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.afd.id
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "origin_group" {
+  name                     = "storage-origin-group"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.afd.id
+
+  load_balancing {}
+
+  health_probe {
+    protocol            = "Https"
+    interval_in_seconds = 120
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "origin" {
+  name                          = "storage-origin"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.origin_group.id
+
+  host_name          = azurerm_storage_account.storage.primary_web_host
+  origin_host_header = azurerm_storage_account.storage.primary_web_host
+
+  http_port  = 80
+  https_port = 443
+
+  certificate_name_check_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_route" "route" {
+  name                          = "route-all"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.endpoint.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.origin_group.id
+
+  cdn_frontdoor_origin_ids = [
+    azurerm_cdn_frontdoor_origin.origin.id
+  ]
+
+  supported_protocols    = ["Http", "Https"]
+  patterns_to_match      = ["/*"]
+  forwarding_protocol    = "HttpsOnly"
+  https_redirect_enabled = true
+
+  link_to_default_domain = true
+}
